f4537562b8629571e7ccee90a354fef3ed6c2a67
DevinAugot <105358840+DevinAugot@users.noreply.github.com>
2023-03-28 15:58:25 -0230

    Update BooksCheckedOut.java

--- src/com/keyin/BooksCheckedOut.java
+++ src/com/keyin/BooksCheckedOut.java
@@ -1,2 +1,29 @@
-package com.keyin;public class BooksCheckedOut {
-}
+package com.keyin;
+
+
+import java.sql.Date;
+
+public class BooksCheckedOut {
+  
+    /* made these private final because I won't need to change anything with the BooksCheckedout
+       because the list is populated from author,patrons books etc  also removed some of the setters and getters because they
+       aren't being used and to clean up code base */
+  
+    private final int checkout_id;
+    private final int book_id;
+    private final int patron_id;
+    private final Date return_date;
+    private final Date checkout_date;
+    private final Date due_date;
+
+
+    public BooksCheckedOut(int checkout_id, int book_id, int patron_id, Date return_date, Date checkout_date,
+                           Date due_date) {
+        this.checkout_id = checkout_id;
+        this.book_id = book_id;
+        this.patron_id = patron_id;
+        this.return_date = return_date;
+        this.checkout_date = checkout_date;
+        this.due_date = due_date;
+
+    }
