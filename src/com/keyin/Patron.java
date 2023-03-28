233b63ff495d6bfbfbd4c6c91e0c73c54ec04286
DevinAugot <105358840+DevinAugot@users.noreply.github.com>
2023-03-28 15:59:33 -0230

    Update Patron.java

--- src/com/keyin/Patron.java
+++ src/com/keyin/Patron.java
@@ -1,2 +1,39 @@
-package com.keyin;public class Patron {
+package com.keyin;
+public class Patron {
+  
+    /* made these private final because I won't need to change anything with the patrons
+       once created for the purpose
+       of this program, also removed some of the setters and getters because they aren't being used and to clean
+       up code base */
+
+    private final int id;
+    private final String firstLastName;
+    private final String address;
+    private final String phoneNum;
+
+    public Patron(int id, String name, String address, String phoneNum) {
+        this.id = id;
+        this.firstLastName = name;
+        this.address = address;
+        this.phoneNum = phoneNum;
+    }
+
+
+    public String getName() {
+        return firstLastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Patron{" +
+                "id=" + id +
+                ", name='" + firstLastName + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNum + '\'' +
+                '}';
+    }
 }
